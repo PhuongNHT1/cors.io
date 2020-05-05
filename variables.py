db4eab305017c1cb6b2f787328a3d02fc8efcefb
Kaoru <phuongnht176@gmail.com>
2020-05-05 22:39:27 +0900

    modified and add

---
+++ variables.py
@@ -0,0 +1,9 @@
+cookies = {'_sp_id.7bc7': '06d67edb75b999999.1466999953.100.1555544393.1234573782',
+           '_sp_ses.7bc7': '*',
+           'csrftoken': 'aHEc9p52paMCKCd29m5lF3wj4W298bLJcRRmCBHXYDwHoXUBj36FwmEqXnmOAHmR',
+           'fbm_143688012353890': 'base_domain=.www.memrise.com',
+           'fbsr_143688012353890': 'bGNeJtlEkaCEISsa4th4J1-FvygfuhFgBuu7qnnS1M.eyJhbGdvcml03f6OiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUNxNTh6ekpDMDlpNGNpTjNyaVRYR3VEN0xHZHM1TEJyMmFCVlJBTmVHeVl4WFlWanM1QVRpMDBtSzFwMWNFV2dxa28wUUxrWUVoT0RKLVdQOG1DdHVncjZhSGdxMVBEaEp3MGhTbV9pRXA3ckcxZU02d2M0LTNubFFCVkVaU0tUVml3eXFKdy1FZmxJYTVuZGJZUlhBXzVlNXdGcFFKWWVJem83ZllwcV9COWExdkJ0S3ZFQlB6OGQ2c2w0azMyOWtxcmFyVjJRYXpodVh3WXVlbVc4ejNlUUc5TE11SXJsakxTbW1hOXN3cUREVzEtcl94WlNLRlRucklsV3FxY3kzN0g1UnFVeTRwOFZ0TVVSXzEzd0Z0TjBrNF9PZjgzalJHYVZjZkV5dWFqUDJLQVhSRkxmT3RrU0R5d3lRSnRncGRMVC1ZZTVRTkNKN0xNRWxfcEVVWSIsImlzc3VlZF9hdCI6MTQ2ODY0Mzk4MiwidXNlcl9pZCI6IjUwMzc3MTY3OCJ9',
+           'i18next': 'en',
+           'ajs_user_id': '1927607',
+           'ajs_anonymous_id': '%22cc1b0ece-5f75-4821-b969-f627da513710%22',
+           'sessionid_2': 'fseu1lx8z7hsu2f9vpbcl5deijvl720t7'}
