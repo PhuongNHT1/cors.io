3fba3ca9914d6017e307284600237bd78d1c53f8
Kaoru <phuongnht176@gmail.com>
2020-05-07 21:00:15 +0900

    remove variables

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from flask import request
-from variables import cookies
 
 import time
 import flask
@@ -12,7 +11,7 @@ import requests
 @app.route('/', methods=("GET", "POST", "OPTIONS"))
 def index():
   qs=request.query_string
-
+  cookies=request.cookies
   if qs:
     try:
       qs = qs.decode('utf8')
